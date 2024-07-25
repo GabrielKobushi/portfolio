17e42bf46df1f5e22572f6330781fdc2f7441b6d
Gabriel R. A. Almeida <gabrielraalmeida@proton.me>
2024-07-25 18:09:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-Olá
-
-minha querida visitante!
-S2
+<?php
+<html>
+   <head>
+      <title>{webpage.title}</title>
+   </head>
+   <body>
+      Olá
+      minha querida visitante!
+      S2
+   </body>
+</html>
+?>
